a44d9c4cf06506b4a5d5cf97b092eea54c295c94
Yi Fong Au <yifongau@gmail.com>
2025-11-01 14:29:29 +0100

    bibliography output

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ bibliographyPage(url)
 async function getPagedItems(url) {
 	const response = await fetch(url);
 	const json = await response.json();
-	console.log(json)
 	return json
 }
 
@@ -46,11 +45,16 @@ function nodePullByValue(sourceNode, sourceKey, targetMap) {
 }
 
 function nodePushIntoTargetKeyByValue(sourceNode, sourceKey, targetMap, targetKey) {
-			const targetId = sourceNode[sourceKey]
-			console.log(targetId)
+			const targetIds = sourceNode[sourceKey]
 
-			
-			targetMap[targetId][targetKey] = sourceNode[sourceKey]
+			for (let x in targetIds) {
+				let targetId = targetIds[x]
+				if (! Object.hasOwn(targetMap[targetId], targetKey)) {
+					nodeAddKey(targetMap[targetId], targetKey, [])
+				}
+
+				targetMap[targetId][targetKey].push(sourceNode);
+			}
 }
 
 function nodeAddKey(node, key, value) {
@@ -66,6 +70,8 @@ function nodeSurfaceNestedKey(node, key, parentKey) {
 // The is the big function
 function campDataSet(campArray, authorArray, titleArray, publisherArray) {
 
+	let campDataSet = [];
+
 	// create nodeMaps of arrays so they can be retrieved by their id
 	let campMap = nodeMapById(campArray);
 	let authorMap = nodeMapById(authorArray);
@@ -75,24 +81,29 @@ function campDataSet(campArray, authorArray, titleArray, publisherArray) {
 	// process titleMap	
 	for (let x in titleMap) {
 		const title = titleMap[x]
-		
+	
+		// add keys to titles
 		nodeSurfaceNestedKey(title, "auteurs", "acf")
 		nodeSurfaceNestedKey(title, "redacteuren", "acf")
 		nodeSurfaceNestedKey(title, "vertalers", "acf")
 		nodeSurfaceNestedKey(title, "bijdragen_auteurs", "acf")
 		nodeSurfaceNestedKey(title, "bijdragen_vertalers", "acf")
+		nodeSurfaceNestedKey(title, "titelinformatie", "acf")
 
-		nodePullByValue(title, "uitgever", publisherMap)
+		// populate keys in titles
 		nodePullByValue(title, "auteurs", authorMap)
 		nodePullByValue(title, "redacteuren", authorMap)
 		nodePullByValue(title, "vertalers", authorMap)
 		nodePullByValue(title, "bijdragen_auteurs", authorMap)
 		nodePullByValue(title, "bijdragen_vertalers", authorMap)
+		nodePullByValue(title, "uitgever", publisherMap)
+
+		// push titles into camps
+		nodePushIntoTargetKeyByValue(title, "kamp", campMap, "titles")
 
 	}
 
-	console.log(campMap)
-	// process campMap
+	// make tree of camp parents and children
 	for (let x in campMap) {
 		const camp = campMap[x]
 
@@ -105,77 +116,117 @@ function campDataSet(campArray, authorArray, titleArray, publisherArray) {
 			campMap[camp.parent].children.push(camp);
 		}
 
-		//
+		// if it has no parents push it into the variable that will be returned
+		// this will work because of copy-by-reference
+		else {
+			campDataSet.push(campMap[camp.id])
+		}
+
+	}
+
+	return campDataSet;
+}
+
+function bibliographyFilter(dataSet) {
+
+	bibliography = []
+
+	for (let x in dataSet) {
+		let dataNode = dataSet[x]
+		console.log(dataNode)
+
+		let object = { name: dataNode.name, titles: [] }
+
+		for (let y in dataNode.titles) {
+			let title = dataNode.titles[y];
+
+			// CREATORS
+			let m = [ title.auteurs, title.redacteuren, title.vertalers, title.bijdragen_auteurs, title.bijdragen_vertalers ]
+
+			// if has author
+			if (m[0]]) {
+
+				if (!m[1] && !m[2]) {
+
+				}
+				// Set creatorString and extraCreatorString
+				else if (m[1] && !m[2]) {
+				}
+
+				else if (m[1] && m[2]) {
+				}
+
+			}
+
+			// if has no author
+			else if (!m[0]) {
+				if (!m[1] && !m[2]) {
+
+				}
+				// Set creatorString and extraCreatorString
+				else if (m[1] && !m[2]) {
+				}
+
+				else if (m[1] && m[2]) {
+				}
+			}
+
+
+
+
+
+				
+				
+
+			
+
+			
+			// NAME OF WORK
+			let titleName = title.titelinformatie;
+			let pubData = title.titelinformatie.publicatiedata;
+			let nameString
+			titleName.ondertitel ? 
+				nameString = titleName.titel.trim() + ": " + titleName.ondertitel : 
+				nameString = titleName.titel
+
+			//  PUBLISHING INFO
+			let titlePubInfo = title.uitgever[0];
+			let PubInfo = `${pubData.plaats_van_publicatie}: ${titlePubInfo.name}, ${pubData.jaar_van_publicatie}.`
+
+
+
+			// COMPOSING BIBTITLE
+			let titleString = nameString + ". " + PubInfo
+			console.log(titleString)
+			object.titles.push(titleString);
+		}
+		bibliography.push(object);
 
 		
-
 	}
 
-	console.log(campMap)
-
-
-
-
-
-
-	return
-
-
-	// Iterate over titleArray and push into corresponding camp in campMap 
-	for (let x in titleArray) {
-		const title = titleArray[x]
-		for (let y in title.kamp) {
-			const campId = title.kamp[y]
-			campMap[campId].posts.push(title);
-
-		}
-
-	}
-
-
-	// Iterate over campArray and create tree in campMap (taking advantage of copy by reference)
-	for (let i in campArray) {
-		const camp = campArray[i]
-		if (camp.parent !== 0) {
-		    campMap[camp.parent].children.push(campMap[camp.id]);
-		}
-	}
-
-	// Iterate over campArray and push items with no parent
-	let treeArray = [];
-	for (let i in campArray) {
-		const camp = campArray[i]
-		if (camp.parent === 0) {
-			treeArray.push(campMap[camp.id])
-		}
-	}
-
-	return treeArray;
+	return bibliography
 }
 
-function bibliographyFilter(treeArray) {
-
-	filteredArray = []
-
-	for (let i in treeArray) {
-		filteredArray.push(treeArray[i].name)
-	}
-
-	return filteredArray
-
-
-}
-
-function renderFilteredArray(filteredArray) {
+function renderFilteredArray(bibliography) {
 
 	let output = document.getElementById('bibliografieOutput')
 
-	for (let i in filteredArray) {
-		const item = filteredArray[i];
+	for (let x in bibliography) {
+		const header = bibliography[x].name;
 		let h1 = document.createElement("h1");
-		h1.appendChild(document.createTextNode(item));
-
+		h1.appendChild(document.createTextNode(header));
 		output.appendChild(h1);
 
+		for (let y in bibliography[x].titles) {
+			const title = bibliography[x].titles[y];
+			let p = document.createElement("p")
+			p.appendChild(document.createTextNode(title));
+			output.appendChild(p);
+		}
+
 	}
+
+
 }
+
